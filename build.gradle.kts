0201213df587f09061defcee38278f32eb703a17
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-09-24 13:26:18 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+group = "me.zeroeightsix"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
