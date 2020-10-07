22b3d1f18682772ee9c870d1f99308076de00137
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-10-07 21:56:14 +0200

    Improve comment on ID

--- src/main/kotlin/me/zeroeightsix/kape/element/IDStack.kt
+++ src/main/kotlin/me/zeroeightsix/kape/element/IDStack.kt
@@ -1,6 +1,10 @@
 package me.zeroeightsix.kape.element
 
-// The JVM provides a hashCode for `Any`, so we're allowed to use any object as ID.
+// The JVM provides a `equals` implementation for Any, so we can assume any type is allowed for IDs.
+// This means that the user can choose if they want to provide e.g. objects to the data types their GUI represents
+// as IDs, titles, or any other data.
+// Kape, by default, uses a `LinkedHashMap` for its ID map - thus, two IDs are identical if, and only if, their
+// `hashCode` implementation produces the same hash.
 typealias ID = Any
 
 typealias IDMap<T> = MutableMap<ID, T>
